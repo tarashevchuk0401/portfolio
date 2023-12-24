b15c7d2470029e5a4a1c4f4c55fecadc13b468d2
Taras Shevchuk <tarashevchuk0401@gmail.com>
2023-12-24 12:32:34 +0100

    email sending done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 // import { EmailJSResponseStatus } from '@emailjs/browser';
-import  emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 import { EmailJSResponseStatus } from '@emailjs/browser';
 
 
@@ -13,30 +13,22 @@ import { EmailJSResponseStatus } from '@emailjs/browser';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent implements OnInit {
 
-  // constructor(private emailjs: emailjs)
-  
-  ngOnInit(): void {
-  }
-  
-  async sendEmail(form: NgForm){
-    console.log(form)
+export class ContactComponent {
+
+  async sendEmail(form: NgForm) {
+    if(form.invalid){
+      return
+    }
     emailjs.init('DfVuStD8G8ujnxSe_');
-    let response = await emailjs.send('service_njktu9h','template_42omheo',{
+    await emailjs.send('service_njktu9h', 'template_42omheo', {
       from_name: form.value.name,
       to_name: 'Taras',
       from_email: form.value.email,
       subject: 'subject',
       message: form.value.message
     })
-    
-    // await emailjs.sendForm('service_njktu9h', 'template_42omheo', body , 'DfVuStD8G8ujnxSe_')
-    // .then((result: EmailJSResponseStatus) => {
-    //   console.log(result.text);
-    // }, (error) => {
-    //   console.log(error.text);
-    // });
-    
+    form.reset();
   }
+
 }
