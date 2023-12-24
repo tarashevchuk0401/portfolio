7581b38ed4348b4827b7493606c8c1c7796ce4b1
Taras Shevchuk <tarashevchuk0401@gmail.com>
2023-12-24 22:17:53 +0100

    responsive disign done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -20,16 +20,35 @@ import { ContactComponent } from './contact/contact.component';
     HomeComponent,
     SidebarComponent,
     PortfolioComponent,
-    ContactComponent
+    ContactComponent,
+    MatIconModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'portfolio';
 
-  navigateTo(element: HTMLElement){
-    const yOffset = element.offsetTop ;
+  constructor(private el: ElementRef) { }
+
+  navigateTo(element: string) {
+    const aboutMe: HTMLElement = this.el.nativeElement.querySelector('.breakpointAboutMe');
+    const portfolio: HTMLElement = this.el.nativeElement.querySelector('.breakpointPortfolio');
+    const contact: HTMLElement = this.el.nativeElement.querySelector('.breakpointContact');
+
+    let yOffset: number = 0;
+    switch (element) {
+      case 'aboutMe':
+        yOffset = aboutMe.offsetTop;
+        break;
+      case 'portfolio':
+        yOffset = portfolio.offsetTop;
+        break;
+      case 'contact':
+        yOffset = contact.offsetTop;
+        break;
+    }
+    
     window.scrollTo({
       top: yOffset
     })
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  @Output() navigateTo = new EventEmitter<string>();
+
+  navigateToElement(element: string) {
+    this.navigateTo.emit(element);
+  }
 
 }
